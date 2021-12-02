04c834d3482047b970f7afb3893683540d4cb7db
James Strande <strandejr@gmail.com>
2021-12-02 18:51:26 -0500

    add user, user login apis added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,38 @@
 const { AuthenticationError } = require('apollo-server-express');
-const Auth = require('../utils/auth');
+const { signToken } = require('../utils/auth');
+const User = require('../models/User');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context) {
+                const myData = await User.findOne({ _id: context.user._id }).select('-__v -password')
+                return myData;
+            }
+
+            throw new AuthenticationError('You need to be logged in to do that!')
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect Credentials')
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user }
+        }
+    }
+}
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    scalar Date
+
+    type User {
+        _id: ID!
+        email: String!
+        username: String
+        eventCount: Int
+        savedEvents: [Event]
+    }
+
+    type Event {
+        eventId: ID!
+        eventName: String!
+        eventDescription: String
+        startDate: String!
+        endDate: String
+        latitude: String
+        longitude: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input EventInput {
+        eventId: String!
+        eventName: String!
+        eventDescription: String
+        startDate: String!
+        endDate: String
+        latitude: String!
+        longitude: String!
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(email: String!, username: String!,  password: String!): Auth
+    }
+`
